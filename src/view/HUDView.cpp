0fb62749d1b2809762327f0cd5f541a6a7498bad
henriheidemann <henri.heidemann@t-online.de>
2025-08-19 23:12:12 +0200

    Removed usage of std::format (not available on JupyterHub)

--- src/view/HUDView.cpp
+++ src/view/HUDView.cpp
@@ -1,7 +1,6 @@
 #include "HUDView.hpp"
 #include "../model/Constants.hpp"
 #include "../model/GameState.hpp"
-#include <format>
 
 // Create HUD and load font
 HUDView::HUDView() :
@@ -23,17 +22,17 @@ void HUDView::draw(Layer& layer, const GameState& state)
 	// Show game information as text and add to layer
 
 	// Score is shown on the left
-	m_score.setString(std::format("Score: {}", state.score));
+	m_score.setString("Score: " + std::to_string(state.score));
 	m_score.setPosition({constants::PADDING, 0.0f});
 	layer.add_to_layer(m_score);
 
 	// Level is shown in the middle
-	m_level.setString(std::format("Level: {}", state.level));
+	m_level.setString("Level: " + std::to_string(state.level));
 	m_level.setPosition({(constants::VIEW_WIDTH - m_level.getLocalBounds().size.x - constants::PADDING)/ 2.0f, 0.0f});
 	layer.add_to_layer(m_level);
 
 	// Number of lives is shown on the right
-	m_lives.setString(std::format("Lives: {}", state.lives));
+	m_lives.setString("Lives: " + std::to_string(state.lives));
 	m_lives.setPosition({constants::VIEW_WIDTH - m_lives.getLocalBounds().size.x - constants::PADDING, 0.0f});
 	layer.add_to_layer(m_lives);
 
